3838016a336b861c6100d56ad3fc2260df6a1a4a
Rostix <puten2050@yandex.ru>
2025-08-25 20:23:15 +0500

    added simple resizing policy

--- ui.py
+++ ui.py
@@ -21,13 +21,11 @@ class Ui_MainWindow(object):
         self.actionConnect_Camera.setObjectName(u"actionConnect_Camera")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.verticalLayoutWidget = QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(0, 0, 791, 541))
-        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.pixmap_label = QLabel(self.verticalLayoutWidget)
+        self.pixmap_label = QLabel(self.centralwidget)
         self.pixmap_label.setObjectName(u"pixmap_label")
         self.pixmap_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
@@ -35,13 +33,13 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.label_2 = QLabel(self.verticalLayoutWidget)
+        self.label_2 = QLabel(self.centralwidget)
         self.label_2.setObjectName(u"label_2")
         self.label_2.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.horizontalLayout_2.addWidget(self.label_2)
 
-        self.pushButton = QPushButton(self.verticalLayoutWidget)
+        self.pushButton = QPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
         self.pushButton.setCheckable(True)
 
@@ -51,13 +49,13 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_4 = QHBoxLayout()
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.label_3 = QLabel(self.verticalLayoutWidget)
+        self.label_3 = QLabel(self.centralwidget)
         self.label_3.setObjectName(u"label_3")
         self.label_3.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.horizontalLayout_4.addWidget(self.label_3)
 
-        self.pushButton_2 = QPushButton(self.verticalLayoutWidget)
+        self.pushButton_2 = QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName(u"pushButton_2")
         self.pushButton_2.setCheckable(True)
 
@@ -67,12 +65,12 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_5 = QHBoxLayout()
         self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
-        self.gamma_label = QLabel(self.verticalLayoutWidget)
+        self.gamma_label = QLabel(self.centralwidget)
         self.gamma_label.setObjectName(u"gamma_label")
 
         self.horizontalLayout_5.addWidget(self.gamma_label)
 
-        self.gamma_slider = QSlider(self.verticalLayoutWidget)
+        self.gamma_slider = QSlider(self.centralwidget)
         self.gamma_slider.setObjectName(u"gamma_slider")
         self.gamma_slider.setOrientation(Qt.Orientation.Horizontal)
 
@@ -83,7 +81,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
         self.horizontalLayout_3.setContentsMargins(250, -1, 250, -1)
-        self.apply_countour_button = QPushButton(self.verticalLayoutWidget)
+        self.apply_countour_button = QPushButton(self.centralwidget)
         self.apply_countour_button.setObjectName(u"apply_countour_button")
         self.apply_countour_button.setCheckable(True)
         self.apply_countour_button.setChecked(False)
@@ -92,10 +90,12 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addLayout(self.horizontalLayout_3)
 
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 23))
+        self.menubar.setGeometry(QRect(0, 0, 800, 33))
         self.menuFunctions = QMenu(self.menubar)
         self.menuFunctions.setObjectName(u"menuFunctions")
         self.menuFIle = QMenu(self.menubar)
@@ -122,19 +122,19 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Area Calc", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Area Calculator", None))
         self.actionCalculate_the_area.setText(QCoreApplication.translate("MainWindow", u"Calculate the area", None))
         self.actionOpen.setText(QCoreApplication.translate("MainWindow", u"Open", None))
         self.actionOpen_Calibration_Image.setText(
             QCoreApplication.translate("MainWindow", u"Open Calibration Image", None))
         self.actionAuto_Gamma_by_area.setText(QCoreApplication.translate("MainWindow", u"Auto Gamma by area", None))
         self.actionConnect_Camera.setText(QCoreApplication.translate("MainWindow", u"Connect Camera", None))
-        self.pixmap_label.setText(QCoreApplication.translate("MainWindow", u"", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"", None))
+        self.pixmap_label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"AutoGamma1", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
         self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"AutoGamma2", None))
-        self.gamma_label.setText(QCoreApplication.translate("MainWindow", u"", None))
+        self.gamma_label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
         self.apply_countour_button.setText(QCoreApplication.translate("MainWindow", u"Apply Contour", None))
         self.menuFunctions.setTitle(QCoreApplication.translate("MainWindow", u"Functions", None))
         self.menuFIle.setTitle(QCoreApplication.translate("MainWindow", u"FIle", None))
