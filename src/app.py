bbe7e866982482d2fc42466c6645c9c9b20998f2
rostix <clavic000@gmail.com>
2026-02-03 23:55:26 +0500

    implemented hikvision support

--- src/app.py
+++ src/app.py
@@ -1,35 +1,42 @@
 import os
 import sys
-import time
 
 from PySide6.QtCore import Qt, Slot, QTimer, QSize
-from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, QProgressDialog, QWidget, QDialog
-from PySide6.QtMultimedia import QCameraDevice, QMediaDevices
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, QProgressDialog, QDialog
+from PySide6.QtMultimedia import QMediaDevices
 
-from ObjectClasses import Image, VideoThread
+from ObjectClasses import Image, VideoThread, HikrobotThread
 from ui import Ui_MainWindow, ChooseCameraDialog
 from utils import OpenCVToQtAdapter
-from mock import HikrobotThread
 
 filename = 'placeholder.png'
 
 
 class ImageViewer(QMainWindow):
     def __init__(self):
-        # inner logic calls and variables
+        super().__init__()
+
+        # Данные изображения
         self.filename = filename
         self.image = Image(filename)
-        super().__init__()
-        self.auto_gamma_flag = False
+        self.processed_image = None
+
+        # Параметры обработки
         self.gamma = 1.0
+        self.auto_gamma_flag = False
         self.first_parameter = 0.7
         self.second_parameter = 0.5
         self.unit_factor = None
-        self.processed_image = None
+        self.unit_name = None
         self.image_init_flag = False
-        self.thread = None
 
-        # Ui calls
+        # Камера
+        self.thread = None
+        self._camera_cti_file = None
+        self._camera_index = None
+        self._is_camera_mode = False  # Режим камеры или файла
+
+        # UI
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.gamma_label.setText(str(self.gamma))
@@ -37,7 +44,6 @@ class ImageViewer(QMainWindow):
         self.ui.gamma_slider.valueChanged.connect(self._slider_move)
         self.ui.actionOpen.triggered.connect(self._open_file)
         self.ui.actionCalculate_the_area.triggered.connect(self._calculate_area)
-        self.ui.apply_countour_button.clicked.connect(self.display_image)
         self.ui.actionConnect_Camera.triggered.connect(self._connect_video_thread)
         self.ui.actionOpen_Calibration_Image.triggered.connect(self._calibrate_area)
         self.ui.pushButton.clicked.connect(self._apply_first_auto_gamma)
@@ -45,81 +51,224 @@ class ImageViewer(QMainWindow):
         self.ui.actionAuto_Gamma_by_area.triggered.connect(self._auto_gamma_by_area)
         self.ui.pixmap_label.setMinimumSize(QSize(200, 200))
         self.ui.actionConnect_cti_file.triggered.connect(self._connect_cti_file)
-        # Don't call display_image() here - wait for the widget to be properly sized
+
+        # Контуры: автоматический стоп-кадр только для камеры
+        self.ui.apply_countour_button.toggled.connect(self._on_contours_toggled)
 
     def showEvent(self, event):
-        """Called when the widget is shown - this ensures proper sizing"""
         super().showEvent(event)
-        # Use a single-shot timer to ensure the widget is fully laid out
         QTimer.singleShot(100, self.display_image)
 
     def resizeEvent(self, event):
-        """Called when the widget is resized - this ensures proper sizing"""
+        super().resizeEvent(event)
         self.display_image()
 
+    # ==================== Обработка контуров ====================
+
+    def _on_contours_toggled(self, checked: bool):
+        """Обработка включения/выключения контуров"""
+        if self._is_camera_mode:
+            # Режим камеры: управляем стоп-кадром
+            if checked:
+                # Включили контуры -> стоп-кадр
+                self._pause_camera()
+            else:
+                # Выключили контуры -> возобновляем видео
+                self._restart_camera()
+        else:
+            # Режим файла: просто п��рерисовываем
+            self.display_image()
+
+    def _pause_camera(self):
+        """Остановка камеры и захват последнего кадра"""
+        if self.thread and self.thread.isRunning():
+            last_image = self.thread.stop()
+            if last_image:
+                self.image = last_image
+        self.display_image()
+
+    def _restart_camera(self):
+        """Перезапуск камеры"""
+        if self._camera_cti_file is not None:
+            # Hikrobot камера
+            self._start_hikrobot_camera()
+        elif self._camera_index is not None:
+            # Обычная камера
+            self._start_video_camera()
+
+    def _start_hikrobot_camera(self):
+        """Запуск Hikrobot камеры"""
+        if self.thread:
+            if self.thread.isRunning():
+                self.thread.stop()
+            self.thread = None
+
+        self.thread = HikrobotThread(self._camera_cti_file, camera_index=self._camera_index)
+        self.thread.error_occurred.connect(lambda msg: print(f"Error: {msg}"))
+        self.thread.frame_ready.connect(self.display_video_slot)
+        self.thread.start()
+
+    def _start_video_camera(self):
+        """Запуск обычной камеры"""
+        if self.thread:
+            if self.thread.isRunning():
+                self.thread.stop()
+            self.thread = None
+
+        self.thread = VideoThread(self._camera_index)
+        self.thread.frame_ready.connect(self.display_video_slot)
+        self.thread.start()
+
+    # ==================== Подключение камер ====================
+
     def _connect_cti_file(self):
-        filename = QFileDialog.getOpenFileName(self, 'Open file', os.getcwd(),
-                                               'Cti File (*.cti)')
-        print(filename)
-        if filename[0]:
-            devices = HikrobotThread.get_devices(filename[0])
-            ids = [device['index'] for device in devices]
-            models = [device['model'] for device in devices]
-            modal = ChooseCameraLogic(parent=self, inputs={'ids': ids, 'names': models})
-            thread_id = modal.get_chosen_camera()
-            if thread_id is not None:
-                if self.thread is not None:
-                    self.thread.stop()
-                    self.ui.delete_snap_button()
-                self.ui.add_snap_button()
-                self.ui.snap_pushButton.clicked.connect(self._snap_camera)
+        """Подключение Hikrobot камеры"""
+        filename = QFileDialog.getOpenFileName(self, 'Open file', '/', 'Cti File (*.cti)')
 
-                self.thread = None
-                self.thread = HikrobotThread(filename[0], thread_id)
-                self.thread.frame_ready.connect(self.display_video_slot)
-                self.thread.start()
+        if not filename[0]:
+            return
 
+        devices = HikrobotThread.get_devices(filename[0])
+        print(devices)
 
-    def _connect_video_thread(self):
+        if not devices:
+            print("No devices found")
+            return
 
-        modal = ChooseCameraLogic(parent=self)
+        ids = [int(device.split(':')[0].split()[1]) for device in devices]
+        models = [device.split(':')[1] for device in devices]
+        modal = ChooseCameraLogic(parent=self, inputs={'ids': ids, 'names': models})
         thread_id = modal.get_chosen_camera()
-        print(thread_id)
+
         if thread_id is not None:
+            # Останавливаем предыдущий поток
             if self.thread is not None:
                 self.thread.stop()
-                self.ui.delete_snap_button()
-            self.ui.add_snap_button()
-            self.ui.snap_pushButton.clicked.connect(self._snap_camera)
 
-            self.thread = None
-            self.thread = VideoThread(thread_id)
-            self.thread.frame_ready.connect(self.display_video_slot)
-            self.thread.start()
+            # Сохраняем параметры камеры
+            self._camera_cti_file = filename[0]
+            self._camera_index = thread_id
+            self._is_camera_mode = True
 
-        print(self.thread)
-    def get_filtered_pixmap(self):
-        image = self.image
-        if self.ui.pushButton.isChecked():
-            image = image.stretch_bright_region(threshold=self.first_parameter)
+            # Сбрасываем контуры
+            self.ui.apply_countour_button.setChecked(False)
 
-        else:
-            image = image.apply_gamma(self.gamma)
-        is_contours = False
-        if self.ui.apply_countour_button.isChecked():
-            image = image.apply_contours()
-            is_contours = True
-        return image.get_pixmap(use_contours=is_contours)
+            # Запускаем камеру
+            self._start_hikrobot_camera()
 
-    @Slot(Image)
+    def _connect_video_thread(self):
+        """Подключение обычной камеры"""
+        modal = ChooseCameraLogic(parent=self)
+        thread_id = modal.get_chosen_camera()
+
+        if thread_id is not None:
+            # Останавливаем предыдущий поток
+            if self.thread is not None:
+                self.thread.stop()
+
+            # Сохраняем параметры камеры
+            self._camera_cti_file = None
+            self._camera_index = thread_id
+            self._is_camera_mode = True
+
+            # Сбрасываем контуры
+            self.ui.apply_countour_button.setChecked(False)
+
+            # Запускаем камеру
+            self._start_video_camera()
+
+    # ==================== Отображение ====================
+
+    @Slot(object)
     def display_video_slot(self, image):
+        """Отображение кадра с камеры"""
         if self.thread is None:
             return
+
+        # Игнорируем кадры если контуры включены (мы на стоп-кадре)
+        if self.ui.apply_countour_button.isChecked():
+            return
+
         self.image = image
-        pixmap = self.get_filtered_pixmap()
-        self.ui.pixmap_label.setPixmap(
-            pixmap.scaled(self.ui.pixmap_label.width(), self.ui.pixmap_label.height(), Qt.KeepAspectRatio,
-                          Qt.SmoothTransformation))
+        pixmap = self._get_pixmap(apply_contours=False)
+        self._set_pixmap(pixmap)
+
+    def display_image(self):
+        """Отображает изображение (файл или стоп-кадр)"""
+        if self.image is None:
+            return
+
+        apply_contours = self.ui.apply_countour_button.isChecked()
+        pixmap = self._get_pixmap(apply_contours=apply_contours)
+        self._set_pixmap(pixmap)
+        self.image_init_flag = True
+
+    def _get_pixmap(self, apply_contours: bool):
+        """Получение pixmap с применением фильтров"""
+        # Выбираем базовое изображение
+        if self.ui.pushButton.isChecked():
+            # Режим stretch_bright_region
+            image = self.image.stretch_bright_region(threshold=self.first_parameter)
+        elif self.processed_image is not None:
+            image = self.processed_image
+        else:
+            image = self.image.apply_gamma(self.gamma)
+
+        # Применяем контуры если нужно
+        if apply_contours:
+            image = image.apply_contours()
+
+        return image.get_pixmap(use_contours=apply_contours)
+
+    def _set_pixmap(self, pixmap):
+        """Установка pixmap с масштабированием"""
+        label = self.ui.pixmap_label
+        if label.width() > 0 and label.height() > 0:
+            scaled = pixmap.scaled(
+                label.width(),
+                label.height(),
+                Qt.KeepAspectRatio,
+                Qt.SmoothTransformation
+            )
+            label.setPixmap(scaled)
+        else:
+            label.setPixmap(pixmap)
+
+    # ==================== Открытие файла ====================
+
+    def _open_file(self):
+        """Открытие файла изображения"""
+        filename = QFileDialog.getOpenFileName(
+            self, 'Open file', os.getcwd(),
+            'Image Files (*.png *.jpg *.bmp)'
+        )
+
+        if filename[0]:
+            # Останавливаем камеру
+            if self.thread is not None:
+                self.thread.stop()
+                self.thread = None
+
+            # Переключаемся в режим файла
+            self._is_camera_mode = False
+            self._camera_cti_file = None
+            self._camera_index = None
+
+            # Загружаем изображение
+            self.filename = filename[0]
+            self.image = Image(self.filename)
+            self.processed_image = None
+
+            self.display_image()
+
+    # ==================== Остальные методы без изменений ====================
+
+    def _slider_move(self):
+        if not self.auto_gamma_flag and self.processed_image is None:
+            gamma = self.ui.gamma_slider.value() / 10.0
+            self.gamma = gamma
+            self.ui.gamma_label.setText(str(gamma))
+        self.display_image()
 
     def _apply_second_auto_gamma(self):
         if self.ui.pushButton_2.isChecked():
@@ -141,94 +290,41 @@ class ImageViewer(QMainWindow):
             self.processed_image = self.image.stretch_bright_region(threshold=self.first_parameter)
         else:
             self.processed_image = None
-
         self.display_image()
-
-    def _slider_move(self):
-        if not self.auto_gamma_flag and self.processed_image is None:
-            gamma = self.ui.gamma_slider.value() / 10.0
-            self.gamma = gamma
-            self.ui.gamma_label.setText(str(gamma))
-        self.display_image()
-
-    def _snap_camera(self):
-        if self.ui.snap_pushButton.isChecked():
-            last_image = self.thread.stop()
-            self.image = last_image
-            pixmap = self.get_filtered_pixmap()
-            self.ui.pixmap_label.setPixmap(
-                pixmap.scaled(self.ui.pixmap_label.width(), self.ui.pixmap_label.height(), Qt.KeepAspectRatio,
-                              Qt.SmoothTransformation))
-        else:
-            self.thread.start()
-
-    def _open_file(self):
-        filename = QFileDialog.getOpenFileName(self, 'Open file', os.getcwd(),
-                                               'Image Files (*.png *.jpg *.bmp)')
-        print(filename)
-        self.ui.delete_snap_button()
-        if filename[0]:
-            if self.thread is not None:
-                self.thread.stop()
-                self.thread = None
-            self.filename = filename[0]
-            self.image = Image(self.filename)
-            self.processed_image = None
-            self.display_image()
 
     def _calibrate_area(self):
-        filename = QFileDialog.getOpenFileName(self, 'Open file', os.getcwd(),
-                                               'Image Files (*.png *.jpg *.bmp)')
-        print(filename)
+        filename = QFileDialog.getOpenFileName(
+            self, 'Open file', os.getcwd(),
+            'Image Files (*.png *.jpg *.bmp)'
+        )
         if filename[0]:
             length, units = OpenCVToQtAdapter.process_calibration_image(filename[0])
             self.unit_factor = int(units[0]) / length
             self.unit_name = units[1]
 
-    def display_image(self):
-        """Отображает изображение OpenCV в виджете"""
-        if self.processed_image is None:
-            gamma_img = self.image.apply_gamma(self.gamma)
-        else:
-            gamma_img = self.processed_image
-        is_contours = False
-        if self.ui.apply_countour_button.isChecked():
-            gamma_img = gamma_img.apply_contours()
-            is_contours = True
-        pixmap = gamma_img.get_pixmap(use_contours=is_contours)
-
-        # Check if the label has proper dimensions before scaling
-        if self.ui.pixmap_label.width() > 0 and self.ui.pixmap_label.height() > 0:
-            # Always scale the image to fit the label properly
-            scaled_pixmap = pixmap.scaled(
-                self.ui.pixmap_label.width(),
-                self.ui.pixmap_label.height(),
-                Qt.KeepAspectRatio,
-                Qt.SmoothTransformation
-            )
-            self.ui.pixmap_label.setPixmap(scaled_pixmap)
-        else:
-            # If label dimensions are not ready, just set the pixmap without scaling
-            self.ui.pixmap_label.setPixmap(pixmap)
-
-        self.image_init_flag = True
-
     def _calculate_area(self):
         message_box = QMessageBox()
         contours = self.image.get_contours()
-        sum_of_areas, areas_units = self.image.calculate_area(
-            self.unit_factor) if self.processed_image is None else self.processed_image.calculate_area(self.unit_factor)
+
+        if self.processed_image is None:
+            sum_of_areas, areas_units = self.image.calculate_area(self.unit_factor)
+        else:
+            sum_of_areas, areas_units = self.processed_image.calculate_area(self.unit_factor)
 
         if areas_units > 0:
-            message_box = QMessageBox()
             message_box.setIcon(QMessageBox.Information)
             message_box.setWindowTitle('Area Calculation')
             message_box.setText(
-                f'Calculated_area: {areas_units:.4f} {self.unit_name} \n{sum_of_areas} px \nAmount_of_contours {len(contours)}')
+                f'Calculated_area: {areas_units:.4f} {self.unit_name}\n'
+                f'{sum_of_areas} px\nAmount_of_contours {len(contours)}'
+            )
         elif sum_of_areas >= 0:
             message_box.setIcon(QMessageBox.Information)
             message_box.setWindowTitle('Area Calculation')
-            message_box.setText(f'Area are not calibrated,\n{sum_of_areas} px \nAmount_of_contours {len(contours)}')
+            message_box.setText(
+                f'Area are not calibrated,\n{sum_of_areas} px\n'
+                f'Amount_of_contours {len(contours)}'
+            )
         else:
             return
         message_box.exec()
@@ -242,6 +338,7 @@ class ImageViewer(QMainWindow):
         progress_dialog.setMaximum(150)
         progress_dialog.setMinimum(0)
         progress_dialog.setCancelButton(None)
+
         image = self.image.clone()
         gamma = image.calculate_gamma_from_contour_graph_with_std(max_gamma=15, modal_window=progress_dialog)
         self.gamma = gamma
@@ -252,13 +349,21 @@ class ImageViewer(QMainWindow):
         self.ui.gamma_label.setText(f'{self.gamma:.2f}')
         self.ui.gamma_slider.setValue(int(self.gamma * 10))
 
+    def closeEvent(self, event):
+        """Закрытие приложения"""
+        if self.thread:
+            self.thread.stop()
+        event.accept()
+
+
 class ChooseCameraLogic(QDialog):
-    def __init__(self, parent=None, inputs:dict=None):
+    def __init__(self, parent=None, inputs: dict = None):
         super().__init__(parent=parent)
         self.setModal(True)
         self.ui = ChooseCameraDialog()
         self.ui.setupUi(self)
         self.ids = []
+
         if inputs is None:
             inputs = QMediaDevices.videoInputs()
             for input in inputs:
@@ -268,13 +373,10 @@ class ChooseCameraLogic(QDialog):
             self.ids = inputs['ids']
             self.ui.comboBox.addItems(inputs['names'])
 
-        print(self.ids)
         self.choose = None
-
         self.ui.buttonBox.accepted.connect(self.accept)
         self.ui.buttonBox.rejected.connect(self.reject)
         self.exec()
-
 
     def accept(self):
         self.choose = self.ids[self.ui.comboBox.currentIndex()]
@@ -283,18 +385,12 @@ class ChooseCameraLogic(QDialog):
     def discard(self):
         self.close()
 
-    def get_chosen_camera(self, parent=None):
+    def get_chosen_camera(self):
         return self.choose
 
 
-
-
-
 if __name__ == "__main__":
-    # ТОЧКА ВХОДА ТУТ
     app = QApplication(sys.argv)
-
     viewer = ImageViewer()
     viewer.show()
-
     sys.exit(app.exec())
